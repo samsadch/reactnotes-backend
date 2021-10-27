b0cb4ae7db7d92b1c75b5639e328038b51196973
Samsad CV <samsad.valappil@thedigitalx.ae>
2021-10-27 20:02:14 +0400

    Changed application properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.postgresql:postgresql")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("mysql:mysql-connector-java")
